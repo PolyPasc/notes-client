611945d7926d772e39d761c73814bb9515794554
user <user@example.com>
2025-03-05 20:01:08 +0530

    deplaoy frontend

---
+++ src/services/ApiEndPoint.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+export const get = (url, params) => instance.get(url, { params });
+export const post = (url, data) => instance.post(url, data);
+export const put = (url, data) => instance.put(url, data);
+export const delet = (url) => instance.delete(url);
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
